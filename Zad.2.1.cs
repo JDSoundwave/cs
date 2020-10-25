46c7b970c0b678abb9360b8640e38290f4c23bc5
Soundwave <60288519+JDSoundwave@users.noreply.github.com>
2020-10-25 16:58:35 +0100

    C#

---
+++ Zad.2.1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cel, stopnie;
+
+            Console.WriteLine("Ile stopni Celsjusza powinienem zamieniæ na stopnie Fahrenheita: ");
+
+            cel = double.Parse(Console.ReadLine());
+
+            stopnie = 32 + (9 / 5) * cel;
+
+            Console.WriteLine(stopnie);
+
+            Console.ReadKey();
+        }
+    }
+}
