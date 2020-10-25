b5aab9d5080b939037f5e2873b70aa5b4eff460f
Soundwave <60288519+JDSoundwave@users.noreply.github.com>
2020-10-25 17:50:19 +0100

    C#

---
+++ Zad.2.4.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Wynikiem bêdzie liczba 202, pre-inkrementacja najpierw zwiêkszy wartoœæ do 101 a dopiero pozniej ja pomnozy przez 2
+
+            int x = 100;
+
+            Console.WriteLine(++x * 2);
+
+            Console.ReadKey();
+        }
+    }
+}
